c5917654c9168e08f37d6872079576a33b52f313
Rodrigo Monteiro <63541433+caprica64@users.noreply.github.com>
2020-12-18 04:53:41 -0300

    Adding initial network config via PowerShell.

--- Installation and Configuration.ps1
+++ Installation and Configuration.ps1
@@ -1,1 +1,29 @@
 
+# Config TCP/IP
+#
+# Find interface index with
+#
+Get-NetAdapter
+#
+# interface index is shown along other information
+#
+# Set IP and GW
+#
+New-NetIPAddress -InterfaceIndex 4 -IPAddress 10.1.1.0 -PrefixLenght 24 - DefaultGateway 10.1.1.1
+#
+#S Set DNS servers
+#
+Set-DnsClientServerAddress - InterfaceIndex 6 - ServerAddress 10.1.1.1
+#
+# Set new hostname
+#
+Hostname
+#
+Rename-Computer -ComputerName currnetComputerName -NewName newComputerName
+#
+# restart the server
+#
+shutdown -r 
+
+
+
